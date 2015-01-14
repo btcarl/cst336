52f42fb72f0e2631ea3d48542fd96013135b54da
btcarl <bcarlston@csumb.edu>
2015-01-13 22:24:09 -0500

    norm

--- CST336/assignments/assignment2/index.php
+++ CST336/assignments/assignment2/index.php
@@ -273,8 +273,8 @@
         <li >
           <span class="sections">Assignments</span>
           <ul>
-              <li class="links"><a href="http://hosting.otterlabs.org/carlstonbriant/CST336/assignments/assignment1/homepage.html">Assignment 1</a></li>
-              <li class="links"><a href="http://hosting.otterlabs.org/carlstonbriant/CST336/assignments/assignment2/index.php">Assignment 2</a></li>
+              <li class="links"><a  href="http://hosting.otterlabs.org/carlstonbriant/CST336/assignments/assignment1/homepage.html">Assignment 1</a></li>
+              <li class="links"><a  style="color:#e91e63" href="http://hosting.otterlabs.org/carlstonbriant/CST336/assignments/assignment2/index.php">Assignment 2</a></li>
           </ul>
         </li>
         <li class="sections">Team Assignment</li>
